27d17d108be2cee968a7c455af24d5745c7ed71d
pawel.lesniewski <pawel.lesniewski@techstep.io>
2025-12-18 16:13:30 +0100

    Fix for distro

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,17 +5,13 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-import { Router as WouterRouter } from "wouter";
 
 function Router() {
   return (
-    <WouterRouter base="/techtrans">
-      <Switch>
-        <Route path={"/"} component={Home} />
-        <Route path={"/404"} component={NotFound} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
+    <Switch>
+      <Route path={"/"} component={Home} />
+      <Route component={Home} />  {/* Wszystkie inne ścieżki idą na Home */}
+    </Switch>
   );
 }
 
